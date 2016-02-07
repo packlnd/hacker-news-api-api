9b924848539ac898cafd416c866144b4c2c8914b
Patrik Ackland <patrikackland@gmail.com>
2016-02-06 22:31:58 -0800

    Gets raw JSON from Github

--- api.py
+++ api.py
@@ -1,5 +1,17 @@
+import urllib2
+import json
+
+#https://developer.github.com/v3/search/
+
 def get_apis():
-    raise NotImplementedError
+    raw_json = get_from_github("hacker-news-api")
+
+def get_from_github(q):
+    api_string = urllib2.urlopen("https://api.github.com/search/repositories?" + \
+                                 "q=" + q + "&" + \
+                                 "sort=stars&" + \
+                                 "order=desc").read()
+    return json.loads(api_string)
 
 def get_endpoints(aid):
     raise NotImplementedError
